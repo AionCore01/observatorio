2828365c4fb29b3ecfb45ec92c6aa9a9d1f41d79
Aion- Orden protocol <pachecormagali@gmail.com>
2026-07-17 01:32:45 -0300

    feat: Initialize observatory/ package structure

---
+++ observatory/__init__.py
@@ -0,0 +1,12 @@
+"""
+Observatorio de Transformaciones Numéricas
+
+Un laboratorio computacional para el estudio de transformaciones numéricas,
+campos temporales y anomalías matemáticas.
+
+Primera piedra: la órbita de Kaprekar.
+Fundado: 2026-07-17
+"""
+
+__version__ = "0.1.0"
+__author__ = "AionCore01"
